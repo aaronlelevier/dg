4c42fb650dea6103498c4a27b8560b90e866a221
aaronlelevier <aaron.lelevier@gmail.com>
2020-09-27 15:19:28 -0700

    JobHandler interface

---
+++ apps/dg_jh/src/dg_jh_interface.erl
@@ -0,0 +1,30 @@
+%%%-------------------------------------------------------------------
+%%% @author Aaron Lelevier
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. Sep 2020 6:45 AM
+%%%-------------------------------------------------------------------
+-module(dg_jh_interface).
+-author("Aaron Lelevier").
+-vsn(1.0).
+
+%% Types
+
+-type name() :: string().
+
+-type result() :: any().
+
+-type reason() :: string().
+
+-type callable() :: fun((M :: atom(), F :: atom(), A :: [any()]) -> result()).
+
+-type jh_request() :: {name(), callable()}.
+
+-type jh_indication() :: {name(), {ok, result()} | {error, reason()}}.
+
+%% Callbacks
+
+-callback submit(pid(), jh_request()) -> ok.
+
+-callback confirm(pid(), jh_indication()) -> ok.
